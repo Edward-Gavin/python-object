fe9aa7ad4cc7c4e1cec8169cd3f2d3056a77c8f9
Gavin <wsw_it_3325@163.com>
2018-04-03 14:51:51 +0800

    add liao.py

---
+++ liao.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+class Student(object):
+
+    def __init(self, name, age):
+        self.name = name
+        self.age = age
+
+    pass
+
+
+if __name__ == "__main__":
+    bart = Student()
+    print(bart)
