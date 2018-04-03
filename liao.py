0f6dc4844bba7dd202ded19841fb288eede6ea18
Gavin <wsw_it_3325@163.com>
2018-04-03 16:45:16 +0800

    add liao.py

--- liao.py
+++ liao.py
@@ -8,9 +8,18 @@ class Student(object):
         self.name = name
         self.score = score
 
-    pass
+    def print_score(self):
+        print('{:d}'.format(self.score))
+
+    def get_grade(self):
+        if self.score >= 90:
+            return 'A'
+        elif self.score >= 60:
+            return 'B'
+        else:
+            return 'C'
 
 
 if __name__ == '__main__':
     bart = Student('shiwei', 24)
-    print(bart.name)
+    print(bart.get_grade())
