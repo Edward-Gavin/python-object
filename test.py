6add03fa60aaa00454cd199bd8a57896b86a81ef
Gavin <wsw_it_3325@163.com>
2018-04-03 14:45:31 +0800

    chang test.py

--- test.py
+++ test.py
@@ -2,53 +2,68 @@
 # -*- coding: utf-8 -*-
 
 
-class Employee:
-    empCount = 0
+class Man:
 
-    def __init__(self, name, salary, age, sex, tall):
-        '''
+    def __init__(self, name, age, sex, tall):
+        """
 
         :param name:
-        :param salary:
         :param age:
         :param sex:
         :param tall:
-        '''
+        """
         self.name = name
-        self.salary = salary
         self.age = age
         self.sex = sex
         self.tall = tall
+
+    @staticmethod
+    def eat(self):
+        print('eat food')
+
+    @staticmethod
+    def sleep(self):
+        print('run ')
+
+
+class Employee(Man):
+    empCount = 0
+
+    def __init__(self, salary):
+        self.salary = salary
+
         Employee.empCount += 1
 
     @staticmethod
     def displaycount():
-        '''
+        """
 
         :return:
-        '''
+        """
         print('total Employee {:d}'.format(Employee.empCount))
 
-
     def displayemployee(self):
-        '''
+        """
 
         :return:
-        '''
+        """
         print('Name: ', self.name, ', Salary: ', self.salary)
 
 
 def print_h(a, b, c):
-    '''
+    """
 
     :param a:
     :param b:
     :param c:
     :return:
-    '''
-    print('hello, world! ')
+    """
+    d = a+b+c
+    print('hello, world! {:d}'.format(d))
 
 
 if __name__ == "__main__":
-    emp = Employee('wang', 23, 25, 'm', 178)
-    print(emp.tall)
+    man = Man('wang', 23, 'M', 178)
+    emp = Employee(23)
+    pass
+
